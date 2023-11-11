16e98834ddf6b6f3ff567d8630790595d57e7052
Jeremy Chang <changjeremy0226@gmail.com>
2023-11-10 16:08:23 -0800

    Vue router init

---
+++ src/components/Counter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <button @click="counter++">{{ counter }}</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const counter = ref(0)
+</script>
