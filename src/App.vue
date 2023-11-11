d397127de9e89fad05a052eee9f48f93a3420a42
Jeremy Chang <changjeremy0226@gmail.com>
2023-11-10 17:50:18 -0800

    Update footer component file

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
     <div>
         <Navigation />
         <router-view></router-view>
+        <MainFooter />
     </div>
 </template>
 
 <script setup>
     import Navigation from '@/components/Navigation.vue'
+    import MainFooter from '@/components/MainFooter.vue'
 </script>
